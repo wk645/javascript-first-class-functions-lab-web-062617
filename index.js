7403eed3047207c20d60fd7bc070ac6bf78549c1
wk645 <wk645@nyu.edu>
2017-08-08 17:14:30 -0400

    first class

---
+++ index.js
@@ -0,0 +1,21 @@
+function countdown(callback) {
+
+	window.setTimeout(callback, 2000)
+}
+
+function createMultiplier(multiplierValue) {
+	return function(value) {
+		return value * multiplierValue
+	}
+}
+
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
+
+function multiplier(first, second) {
+	return (first * second)
+}
+
+var doublerWithBind = multiplier.bind(null, 2)
+var triplerWithBind = multiplier.bind(null, 3)
+
